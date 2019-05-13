ca84f0d933b70307ce919cce8e695b25a32a710c
Adam Ruka <adamruka@amazon.com>
2019-05-13 14:50:39 -0700

    Basic CodePipeline that builds the Docker image (no deploy yet).

--- http-service/bin/http-service.ts
+++ http-service/bin/http-service.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/cdk');
 import { HttpServiceStack } from '../lib/http-service-stack';
 import { ClusterStack } from '../lib/cluster-stack';
+import { CodePipelineStack } from '../lib/code-pipeline-stack';
 
 const app = new cdk.App();
 const clusterStack = new ClusterStack(app, 'ClusterStack');
@@ -11,3 +12,5 @@ new HttpServiceStack(app, 'HttpServiceStack', {
   vpc: clusterStack.vpc,
   cluster: clusterStack.cluster,
 });
+
+new CodePipelineStack(app, 'CodePipelineStack');
